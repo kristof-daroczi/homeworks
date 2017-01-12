6d599d0f214feda6c84c05d318b953adb31c3601
Metalhulk <dkristoff@hotmail.com>
2017-01-12 14:42:08 +0100

    Logger fixed

--- javaresthw/javaresthw-web/src/main/java/xyz/codingmentor/javaresthw/errors/GeneralExceptionMapper.java
+++ javaresthw/javaresthw-web/src/main/java/xyz/codingmentor/javaresthw/errors/GeneralExceptionMapper.java
@@ -2,23 +2,23 @@ package xyz.codingmentor.javaresthw.errors;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.inject.Inject;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
 
 /**
  *
  * @author Krisz
  */
+@Provider
 public class GeneralExceptionMapper implements ExceptionMapper<Throwable> {
 
-    @Inject
-    private Logger logger;
+    private static final Logger LOGGER = Logger.getLogger(GeneralExceptionMapper.class.getSimpleName());
 
     @Override
     public Response toResponse(Throwable exception) {
-        logger.log(Level.SEVERE, "General Exception", exception);
+        LOGGER.log(Level.SEVERE, "General Exception", exception);
         return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
                 .entity(new ErrorDTO(exception.getMessage() + " - " + exception.getCause()))
                 .type(MediaType.APPLICATION_JSON).build();
